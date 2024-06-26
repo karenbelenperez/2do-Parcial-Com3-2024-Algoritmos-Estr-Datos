d4e11f36976dea75241e839528de5ebf943194ea
karenbelenperez <k.beelenperez@gmail.com>
2024-06-26 20:48:38 -0300

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,23 @@
+class fecha: 
+  def __init__(self, día, mes, año):
+    self.día = día
+    self.mes  = mes
+    self.año = año 
+def __srt__(self):
+  return f"[self.día]/[self.mes]/[self.año]"
+
+def __add__(self, días)
+  nueva.fecha = datetime(self.año, self.mes, self.día)+ timedelta(days=días)
+  return fecha(nueva_fecha.day, nueva_fecha.month, nueva_fecha.year)
+
+def __eq__ (self, otra_fecha):
+  return(self.día == otra_fecha.día and
+         self.mes == otra_fecha.mes and
+         self.año == otra_fecha.año)
+  
+def calcular_dif_fecha (self, otra_fecha):
+  fecha1 ==datetime(self.año, self.mes, self.día)
+  fecha2 == datetime(otra_fecha.año, otra_fecha.mes, otra_fecha.día)
+  diferencia == abs ((fecha dos - fecha uno). days)
+    return diferencia
 
