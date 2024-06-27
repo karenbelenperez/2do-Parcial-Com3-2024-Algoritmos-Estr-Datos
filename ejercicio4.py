a36877ba43e79e9e88f677cb20604ff9752e8796
karenbelenperez <k.beelenperez@gmail.com>
2024-06-26 22:13:36 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,1 +1,22 @@
+#ordenamiento de burbuja
+def ordenar_por_fecha_ingreso (self):
+   if not self.cabeza or not self.cabeza.siguiente:
+     return
+      cambio = true
+     while cambio: 
+      cambio = false
+      actual = self.cabeza
+      while actual.siguiente:
+        if actual.dato.datos["fechaingreso"] > actual.siguiente.dato.datos["fechaingreso"]:
+          #se procede a intercambiar los datos de los nodos.
+          actual.dato, actual.siguiente.dato = actual.siguiente.dato, actual.dato
+          cambio = true
+          actual = actual.siguiente
+          
+
+
+
+
+
+               
 
