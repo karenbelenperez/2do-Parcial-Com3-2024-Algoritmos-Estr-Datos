478b475d2a5458c1658ad34ee504ee994b2e49b5
karenbelenperez <k.beelenperez@gmail.com>
2024-06-26 23:08:36 -0300

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -8,18 +8,17 @@ def guardar_lista_en_archivo(lista,archivo):
     with open(archivo, "w") as file: 
          json.dump([alumno.datos for alumno in lista], file, default=str)
        
-
 def mover_directorio(origen, destino):
-    try:
-        os.rename(origen, destino)
-    except Exception as e:
-        print(f"Error al mover el directorio: {e}")
-
+   try: 
+       os.rename(origen, destino)
+   exceot exception as e:
+       print(f"error al mover el directorio: [e]")
 def borrar_archivo_y_directorio(archivo, directorio):
-    try:
+    try
         if os.path.exists(archivo):
             os.remove(archivo)
         if os.path.exists(directorio):
             os.rmdir(directorio)
-    except Exception as e:
-        print(f"Error al borrar el archivo o directorio: {e}")
+    except exception as e:
+        print(f"error al borrar el archivo o directorio: [e]")
+       
