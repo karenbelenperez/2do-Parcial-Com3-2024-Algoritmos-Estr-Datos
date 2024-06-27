dcc8e745522f1e9434a69168775efadafdcfb8c4
karenbelenperez <k.beelenperez@gmail.com>
2024-06-26 22:38:52 -0300

    Update ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,19 +1,20 @@
-def guardar_lista_en_archivo(lista, archivo):
-    with open(archivo, 'w') as file:
-        json.dump([alumno.datos for alumno in lista], file, default=str)
+# Nuevo directorio
+directorio = "directorio_alumnos"
+if not os.path.exists(directorio):
+    os.mkdir(directorio)
+    
+# Creación de lista y guardar
 
-def mover_directorio(origen, destino):
-    try:
-        os.rename(origen, destino)
-    except Exception as e:
-        print(f"Error al mover el directorio: {e}")
+lista_alumnos = ListaDoblementeEnlazada()
+lista_alumnos.lista_ejemplo()
+archivo_alumnos = os.path.join(directorio, 'lista_alumnos.json')
+guardar_lista_en_archivo(lista_alumnos, archivo_alumnos)
 
-def borrar_archivo_y_directorio(archivo, directorio):
-    try:
-        if os.path.exists(archivo):
-            os.remove(archivo)
-        if os.path.exists(directorio):
-            os.rmdir(directorio)
-    except Exception as e:
-        print(f"Error al borrar el archivo o directorio: {e}")
+# Mover el directorio a una nueva ruta
+nueva_ruta = 'nueva_ruta_alumnos'
+mover_directorio(directorio, nueva_ruta)
+
+# Borrar el archivo y el directorio en la nueva ruta
+nuevo_archivo_alumnos = os.path.join(nueva_ruta, 'lista_alumnos.json')
+borrar_archivo_y_directorio(nuevo_archivo_alumnos, nueva_ruta)
 
