66542625125cafe7a39d39e7ae0079d01faa453d
karenbelenperez <k.beelenperez@gmail.com>
2024-06-26 21:33:33 -0300

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,43 @@
+class fecha:
+ def __init__(self, día, mes, año):
+   self.día = día
+   self.mes = mes
+   self.año = año
+
+def __srt__(self):
+ return f"[self.día]/[self.mes]/[self.año]"
+
+def __eq__(self, otra_fecha):
+   return (self.día == otra_fecha.día and
+           self.mes == otra_fecha.mes and
+           self.año == otra_fecha.año)
+
+class alumno:
+  def __init__(self, nombre, dni, fecha_ingreso, carrera):
+     self.nombre = nombre
+     self.dni = dni
+     self.fecha_ingreso = fecha_ingreso
+     self.carrera = carrera
+
+    
+ def cambiar_datos(self, nuevos_datos):
+    for key. value in nuevo_datos.items():
+      if key in self.datos:
+        self.datos[key] = value
+        
+def antiguedad(self):
+    fecha_ingreso = self.dato[fecha_ingreso]
+    fecha_ingreso_dt = datetime(fecha_ingreso.año, fecha_ingreso.mes, fecha_ingreso.día)
+    hoy = datetime.today()
+    dif = hoy-fecha_ingreso_dt
+    return dif.days
+
+def __str__(self):
+    return (f"nombre: [self.datos["nombre"]]/n"
+            f"dni:  [self.datos["dni"]]/n"
+            f"fecha_ingreso: [self.datos["fecha_ingreso"]]/n"
+            f"carrera: [self.datos["carrera"]]/n")
+
+ def __eq__(self, otro_alumno):
+   return self.datos["dni"] == otr_alumno.datos["dni"]
 
